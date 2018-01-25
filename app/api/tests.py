f5fa9f6df734acb985e26b4b90f4ca3fa52f2022
Jeremy Jones <jer@jerjones.me>
2018-01-25 16:04:18 +0000

    apistar setup, cleanup & tests

--- app/api/tests.py
+++ app/api/tests.py
@@ -1,20 +1,44 @@
 from apistar.test import TestClient
-from app import app, welcome
+from app import app
+from project.routes import hello
 
 
-def test_welcome():
+def default_page_return() -> dict:
+    """
+    Single copy of the string returned by the default home page.
+    """
+    return {'message': "Welcome to the future, mystery person!"}
+
+
+def test_hello() -> bool:
     """
     Testing a view directly.
     """
-    data = welcome()
-    assert data == {'message': 'Welcome to API Star!'}
+    data = hello()
+    assert data == default_page_return()
 
+def get_json_url(url, returnResponse=False) -> dict:
+    """
+    Return the json content returned from a URL, parsed to a dict.
+    """
+    client = TestClient(app)
+    response = client.get(url)
+    return response if returnResponse else response.json()
 
-def test_http_request():
+def test_http_request() -> bool:
     """
     Testing a view, using the test client.
     """
-    client = TestClient(app)
-    response = client.get('http://localhost/')
+    response = get_json_url('http://localhost:8080/',
+                            returnResponse=True)
     assert response.status_code == 200
-    assert response.json() == {'message': 'Welcome to API Star!'}
+    assert response.json() == default_page_return()
+
+def test_proxy_http_request() -> bool:
+    """
+    Same test view, using the client but through port 80.
+    """
+    response = get_json_url('http://localhost/',
+                            returnResponse=True)
+    assert response.status_code == 200
+    assert response.json() == default_page_return()
