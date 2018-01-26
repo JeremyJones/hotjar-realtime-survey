9c6c417eb6c54a89af22c5bf775d6a3b4162ed9c
Jeremy Jones <jer@jerjones.me>
2018-01-26 12:02:23 +0000

    settings object

--- app/project/settings/__init__.py
+++ app/project/settings/__init__.py
@@ -1,4 +1,18 @@
+from apistar import environment, typesystem
+
+
+class Env(environment.Environment):
+    properties = {
+        'DEBUG': typesystem.boolean(default=False),
+        'DATABASE_URL': typesystem.string(default='sqlite://')
+    }
+
+env = Env()
+
 settings = {
+    'DATABASE': {
+        'URL': env['DATABASE_URL']
+    },
     'TEMPLATES': {'ROOT_DIR': 'templates',
                   'PACKAGE_DIRS': ['apistar']
     },
