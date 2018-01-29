aa61eebc6680093289390e7ceccadda4f3e2b8a6
Jeremy Jones <jeremagne@gmail.com>
2018-01-29 20:16:40 +0000

    regular, js work on loading dashboard, google viz in on that dashboard page

--- app/lib/dashboard.js
+++ app/lib/dashboard.js
@@ -1,12 +1,12 @@
-var app = angular.module("dashboard", []);
+var newapp = angular.module("dashboard", []);
 
-app.config(['$interpolateProvider', function($interpolateProvider) {
+newapp.config(['$interpolateProvider', function($interpolateProvider) {
     $interpolateProvider.startSymbol('{ng ');
     $interpolateProvider.endSymbol(' ng}');
 }]);
 
 
-app.controller("dashboardCtrl", function($scope) {
+newapp.controller("dashboardCtrl", function($scope) {
 
     $scope.message = "Hello you guys";
 
@@ -15,3 +15,42 @@ app.controller("dashboardCtrl", function($scope) {
         $scope.fooname = "hello " + $scope.name;
     };
 });
+
+
+
+var app = {
+
+    responses: null,
+
+    start: function () {
+	app.fillResponses(function () {
+		app.log("Ready");
+	    });
+    },
+    
+    fillResponses: function (cb=null) {
+	$.ajax({url: "/responses",
+		param_cb: cb,
+		success: function (d) {
+		    app.responses = d._items;
+		    app.log("Assigned responses");
+		    app.log("my responses are " + app.responses.length + " long");
+
+		    if ("undefined" != typeof(this.param_cb))
+			this.param_cb();
+		}});
+    },
+    
+    log: function (m) {
+	console.log(m);
+    }
+}
+    
+google.charts.load('current', {'packages':['table']});
+//google.charts.setOnLoadCallback(drawTable);
+google.charts.setOnLoadCallback(app.start);
+	    
+//Backbone.Collection.extend({url:'/responses'});
+//responses.fetch();
+
+
