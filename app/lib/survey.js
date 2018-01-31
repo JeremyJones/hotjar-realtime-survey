00e5c28e5cb36acf951f0fa26792bf4e62ab4b6e
Jeremy Jones <jer@jerjones.me>
2018-01-31 10:45:40 +0000

    survey js

--- app/lib/survey.js
+++ app/lib/survey.js
@@ -1,6 +1,26 @@
-var app = angular.module("survey", []);
+var newapp = angular.module("survey", []);
 
-app.config(['$interpolateProvider', function($interpolateProvider) {
+newapp.config(['$interpolateProvider', function($interpolateProvider) {
     $interpolateProvider.startSymbol('{ng ');
     $interpolateProvider.endSymbol(' ng}');
 }]);
+
+
+//
+
+
+var app = {
+
+    /*
+
+    */
+
+    "getQuestions": function() {
+        $.ajax({
+            'url': '/questions',
+            'success': function() {
+                app.log("Got questions");
+            }
+        });
+    }
+};
