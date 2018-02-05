3b6e7ad049584cec26b62971c26f2395b64f84e7
Jeremy Jones <jeremagne@gmail.com>
2018-02-05 12:35:31 +0000

    on string validation, require a letter or a number, not just a non-zero length

--- app/project/utils/validators/valid_string/__init__.py
+++ app/project/utils/validators/valid_string/__init__.py
@@ -1,4 +1,6 @@
+from re import match
+
 def valid_string(check_str: str) -> bool:
     """Check whether a string adheres to our rules. Currently non-zero length.
     """
-    return len(check_str) > 0
+    return len(check_str) > 0 and match(r'[a-zA-Z0-9]', check_str)
